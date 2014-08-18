84a1f49e2881d94f8f7d6ca7bda661fa075aee69
Hyacinthe Cartiaux <Hyacinthe.Cartiaux@uni.lu>
2014-08-18 18:51:14 +0200

    [network] Fix for Centos/Redhat 6

--- manifests/definitions/network-interface.pp
+++ manifests/definitions/network-interface.pp
@@ -66,7 +66,7 @@ define network::interface(
         fail("Wrong format in the configuration of the network interface ${interface}")
     }
 
-    if ( ($pre_up or $post_up or $pre_down or $post_down)
+    if ( ($pre_up != [ ] or $post_up != [ ] or $pre_down != [ ] or $post_down != [ ])
           and ! ($::operatingsystem in [ 'Debian', 'Ubuntu' ])
        ) {
         fail("pre_up, post_up, pre_down and post_down parameters are supported only on debian and ubuntu systems")
